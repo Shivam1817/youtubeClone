77ce6688b931d00f5bddc3e026d85e373b439cdb
Shivam <shivam.chem21@iitg.ac.in>
2025-01-22 01:37:42 +0530

    deploying on github

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import MainContent from './components/MainContent';
 import SearchPage from './pages/SearchPage';
